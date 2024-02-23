7ee1e7148089b884a3da9cf5409fa37320771e85
Jackie <actions@github.com>
2024-02-23 17:04:42 -0500

    Complete

--- src/Page/Recipes/Recipes.jsx
+++ src/Page/Recipes/Recipes.jsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react';
 import './Recipes.css';
 import axios from 'axios';
 import arrow from '../../Asset/UI/arrow-right.png'
-import Countdown from '../../Component/Countdown/Countdown';
 import Newsletter from '../../Component/Newsletter/Newsletter';
 
 const Recipes = () => {
   const [recipes, setRecipes] = useState([]);
   const [activeIndex, setActiveIndex] = useState()
+
   const [tag, setTag] = useState('chicken')
   const categories = ['Chicken', 'Beef', 'Vegan', 'Noodles', 'Soup', 'Salad', 'Dessert'];
 
@@ -18,14 +18,14 @@ const Recipes = () => {
       url: 'https://food-recipes-with-images.p.rapidapi.com/',
       params: {q: query},
       headers: {
-        'X-RapidAPI-Key': '11508c4a15msh9a6b1b310d5c085p1f0406jsn3028f9f76d6e',
+        'X-RapidAPI-Key': 'cb578e34aemshb27d4bdad3e28a3p1e7882jsn26dc61fb50d2',
         'X-RapidAPI-Host': 'food-recipes-with-images.p.rapidapi.com'
       }
     };
 
     try {
       const response = await axios.request(options);
-      setRecipes((response.data.d).slice(0, 12))
+      setRecipes((response.data.d).slice(0, 40))
     } catch (error) {
       console.error(error);
     }
@@ -40,13 +40,13 @@ const Recipes = () => {
     <div className='recipes'>
       <div className="banner">
         <div className="hero-content">
-          <h1>Our In House Recipes</h1>
-          <h3>Scroll Down to Get Inspired</h3>
+          <h1>This Month's Menu</h1>
+          <h3>Scroll Down for More</h3>
         </div>
       </div>
 
       <div className="container">
-        <h2>Explore Trending Tags</h2>
+        <h2>Explore by Category</h2>
         <div>
           <ul className='tags'>
             {categories.map((category) => (
@@ -57,16 +57,28 @@ const Recipes = () => {
         <div className="recipes-list">
           {recipes.map((recipe, index) => (
             <div key={index} className={index===activeIndex ? 'recipe-wrapper active': 'recipe-wrapper'}>
-              <img src={recipe.Image} alt="" onMouseEnter={()=>setActiveIndex(index)} onMouseLeave={()=>setActiveIndex(-1)}/>
-              <h4>{recipe.Title} <img src={arrow} alt="" className='arrow hidden'/></h4>
-              <div className="content">
-                <span>Ingredients:</span>
-                <ul>
-                  {Object.values(recipe.Ingredients).slice(0,3).map((ingredient, idx) => (
+              <img className='recipe-image' src={recipe.Image} alt=""  />
+              <div>
+                <h4>{recipe.Title} </h4>
+                <div className="fs-show">
+                    
+                    <ul>
+                  {Object.values(recipe.Ingredients).slice(0,4).map((ingredient, idx) => (
                     <li key={idx}>{ingredient}</li>
                   ))}
                 </ul>
-                <p>{recipe.Instructions.slice(0,85)} <span>see full recipe</span></p>
+                </div>
+              </div>
+      
+              <div className="content">
+                <span className='bs-hide'>Ingredients:</span>
+                <ul className='bs-hide'>
+                  {Object.values(recipe.Ingredients).slice(0,4).map((ingredient, idx) => (
+                    <li key={idx}>{ingredient}</li>
+                  ))}
+                </ul>
+                <span>Instructions</span>
+                <p>{recipe.Instructions}</p>
               </div>
 
             </div>
